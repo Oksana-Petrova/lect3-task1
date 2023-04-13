1a662b2ae599a4886f77dd609be3aedac76d50a7
Oksana Petrova <petrova-2106@mail.ru>
2023-04-13 21:26:35 +0300

    version 0

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 15730;
+        int rubMile = 20;
+        int bonusMile = (amount / rubMile);
+
+        System.out.println("Количество бонусных миль: " + bonusMile);
+    }
+}
