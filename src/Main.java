b4db0c1e0818f978aa09b24b3f0b225d3c8e8033
Oksana Petrova <petrova-2106@mail.ru>
2023-04-14 19:30:30 +0300

    Project change

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
         int amount = 15730;
         int rubMile = 20;
         int bonusMile = (amount / rubMile);
-
         System.out.println("Количество бонусных миль: " + bonusMile);
     }
 }
